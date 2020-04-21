2f92c5903fcc6d0695a943b5567f87b78c9a807a
Tzvi (Andrew) Merczynski-Hait <andrewmh20@users.noreply.github.com>
2020-04-21 15:58:23 -0400

    basic vertical layout

--- main.cpp
+++ main.cpp
@@ -9,16 +9,37 @@ int main() {
     Glyph_string greeting{"Hello, World!", Attribute::Underline};
 
     // Create Textbox Widget with `greeting` as the initial text.
-    Textbox tb{greeting};
+    // Textbox tb{greeting};
+
+    // Menu menu{"menu"};
+    // menu.append_item("test");
+    // Textbox & tb = menu.make_child<Textbox>(greeting);
+    layout::Vertical v{};
+    v.make_child<Menu>("menu V Test");
+    v.make_child<Textbox>(greeting);
+
+    layout::Stack stack{};
+
+    stack.make_page<Menu>("menu Test");
+    stack.make_page<Textbox>(greeting);
+    stack.set_active_page(1);
+    
+    v.enable();
+
+    stack.enable();
+    // v.enable();
+    // tb.enable();
+    // menu.enable();
+
 
     // Set the background and foreground colors of the Textbox.
-    tb.brush.set_background(Color::Dark_blue);
-    tb.brush.set_foreground(Color::Light_blue);
+    // tb.brush.set_background(Color::Dark_blue);
+    // tb.brush.set_foreground(Color::Light_blue);
 
     // Enable a border to be drawn around the Textbox.
-    tb.border.enable();
+    // tb.border.enable();
 
     // Set the Textbox as the head(top-level) Widget, initialize the screen and
     // start the user input event loop.
-    return sys.run(tb);
+    return sys.run(v);
 }
