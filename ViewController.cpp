732bdf00711213802eb72028eee2154cdfe21f14
Tzvi (Andrew) Merczynski-Hait <andrewmh20@users.noreply.github.com>
2020-04-26 01:29:22 -0400

    Not compiling, ViewController.cpp demonstrate const issue

--- ViewController.cpp
+++ ViewController.cpp
@@ -4,3 +4,11 @@
 ViewController::ViewController(const Serial& serial)
 : serial{serial}
 {}
+
+std::string ViewController::checkForData() const{
+    ioMode = ViewController::HEX;
+}
+
+void ViewController::disconnect(){
+        ioMode = ViewController::HEX;
+}
