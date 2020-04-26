f47bdb4b4c637c8f3417881fd0feb00360cdf121
Max Goldman <max@zenoradio.com>
2020-04-26 17:20:24 -0400

    More UI changes

--- SerialTextInputView.cpp
+++ SerialTextInputView.cpp
@@ -3,6 +3,7 @@
 SerialTextInputView::SerialTextInputView() {
   this->border.segments.disable_all();
   this->border.segments.east.enable();
+  this->border.segments.south.enable();
   this->border.enable();
 
   this->height_policy.maximum(4);
