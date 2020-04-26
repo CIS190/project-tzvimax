0bbd265f51e1c58ab8a59d8501fccd0ff85e7db0
Tzvi (Andrew) Merczynski-Hait <andrewmh20@users.noreply.github.com>
2020-04-25 22:16:48 -0400

    Delete uneeded files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tzvi/Dropbox/Dropbox_Documents/Upenn/CIS190/project-tzvimax
-# Build directory: /Users/tzvi/Dropbox/Dropbox_Documents/Upenn/CIS190/project-tzvimax
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("CPPurses")
