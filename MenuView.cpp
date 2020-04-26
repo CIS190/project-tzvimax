365dda13143e7bc896785238664b78013b27210c
Max Goldman <max@zenoradio.com>
2020-04-26 14:10:21 -0400

    Update UI

--- MenuView.cpp
+++ MenuView.cpp
@@ -1,7 +1,13 @@
 #include "MenuView.hpp"
 
 
-MenuView::MenuView(): Menu("Serial Menu") {
+MenuView::MenuView(): Menu("Serial Display") {
   this->append_item("Connect");
   this->append_item("Disconnect");
+
+  this->height_policy.maximum(8);
+
+  this->border.segments.east.disable();
+  this->border.segments.west.disable();
+  this->border.enable();
 }
